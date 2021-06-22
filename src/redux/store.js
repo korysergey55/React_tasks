12c8632f0efafc70ea604e6109366ef2a8d39ea4
AndriiShnurkov <Shans12.84@ukr.net>
2021-06-22 14:24:11 +0300

    added clg

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,5 +3,6 @@ import rootReducer from "./rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const store = createStore(rootReducer, composeWithDevTools());
+console.log("store :>> ", store);
 
 export default store;
